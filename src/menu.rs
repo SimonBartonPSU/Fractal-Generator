a30a783bfc05e36536e003e29d06ea4b28b4b08c
Liam Rotchford <liamrotchford@gmail.com>
2019-08-03 19:04:14 -0700

    Implemented separated functions for color prints and match
    - Altered normal menu to call functions for the printed prompts and the match 
    - implemented it onto the solid background option
    - more to change

--- src/menu.rs
+++ src/menu.rs
@@ -14,7 +14,7 @@ pub fn user_menu(mut scheme: &mut Scheme) {
     \n\nUSER MENU: \n
     \to What type of background would you like? Please select from the following options. \n
     \t\t1) Normal: Simple black background\n
-    \t\t2) Custom: User choice color background\n
+    \t\t2) Custom: User choice colored background\n
     \t\t3) Random: Randomly generated background\n
     \to Input: "
     );
@@ -31,7 +31,6 @@ pub fn user_menu(mut scheme: &mut Scheme) {
     match trimmed {
         "normal" | "1" => normal_menu(&mut scheme),
         "custom" | "2" => {
-            println!("We're glad you chose customization message\n\n");
             normal_menu(&mut scheme);
             custom_menu(&mut scheme)
         }
@@ -52,8 +51,90 @@ pub fn normal_menu(mut scheme: &mut Scheme) {
 
     if scheme.fractal == "barnsley" {
         scheme_type = true;
-        print!(
-            "
+        input = color_options_extensive();
+
+    } else {
+        scheme_type = false;
+        input = color_options_rgb();
+    }
+
+    input = color_determine(input, scheme_type);
+
+    let color: &str = &input;
+
+    scheme.color = str_to_color(color);
+
+    println!("\n========================================================================================================================================\n");
+}
+
+/// The custom option allows a user to fine tune properties
+/// of the fractal art image.
+pub fn custom_menu(mut scheme: &mut Scheme) {
+    
+    let mut input = String::new();  
+    
+    print!(
+        "\n\no BACKGROUND STYLE MENU: \n
+        \to Which background coloring style would you like? Please select from the options below \n
+        \t\t1) Solid: solid colored background\n
+        \t\t2) Transitional: multiple color transition background\n
+        \to Input: "
+    );
+   
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).ok();
+    let mut trimmed: &str = &input.trim().to_lowercase();
+    
+    println!("\n========================================================================================================================================\n");
+
+    match trimmed {
+        "1" => {
+
+            println!("\n\no SOLID BACKGROUND COLOR MENU: \n
+                \to What solid color background would you like? Please select from the options below \n ");
+            
+            input = color_options_extensive();
+
+            input = color_determine(input, true);
+
+            let color: &str = &input;
+
+            scheme.bg_color = str_to_color(color);
+        }
+
+        "2" => {
+            scheme.fancy_background = true;
+
+            input.clear();
+
+            println!("Choose your first color: red, green, blue: ");
+
+            io::stdin().read_line(&mut input).ok();
+
+            scheme.bg_color = str_to_color(input.trim());
+
+            input.clear();
+
+            println!("Choose one of the remaining two: red, green, blue: ");
+
+            io::stdin().read_line(&mut input).ok();
+
+            scheme.bg_color_2 = str_to_color(input.trim());
+
+            println!("Assummed good input...");
+        }
+
+        
+        _ => println!("Invalid input: {:?}. Enter a number (1, 2, ..)", input),
+    }
+        
+    input.clear();
+}
+
+pub fn color_options_extensive() -> String {
+    let mut input = String::new();  
+
+    print!("
         \t1) Red\n
         \t2) Orange\n
         \t3) Yellow\n
@@ -62,21 +143,30 @@ pub fn normal_menu(mut scheme: &mut Scheme) {
         \t6) Violet\n
         \t7) White\n
         o Input: "
-        );
-    } else {
-        scheme_type = false;
-        print!(
-            "
+    );
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).ok();
+    
+    input.to_string()
+}
+
+pub fn color_options_rgb() -> String {
+    let mut input = String::new();  
+
+    print!("
         \t1) Red\n
         \t2) Green\n
         \t3) Blue\n
         \t4) White\n
         o Input: "
-        );
-    }
-
+    );
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut input).ok();
+    
+    input.to_string()
+}
+
+pub fn color_determine(input: String, scheme_type: bool) -> String {
 
     let mut trimmed: &str = &input.trim().to_lowercase();
 
@@ -109,47 +199,7 @@ pub fn normal_menu(mut scheme: &mut Scheme) {
         _ => trimmed, //if not a numeric char then just keep input as is
     };
 
-    scheme.color = str_to_color(trimmed);
-
-    println!("\n========================================================================================================================================\n");
-}
-
-/// The custom option allows a user to fine tune properties
-/// of the fractal art image.
-pub fn custom_menu(mut scheme: &mut Scheme) {
-    let mut buffer = String::new();
-    let std = io::stdin();
-
-    let mut finished: bool = false;
-    while !finished {
-        println!("Select an item to customize by its ID number:");
-        println!("    1. Background color (solid)\n    2. Background color (transition)\n    'quit' to quit\n");
-        std.read_line(&mut buffer).ok();
-
-        match buffer.trim() {
-            "1" => {
-                buffer.clear();
-                println!("What color background would you like? ");
-                io::stdin().read_line(&mut buffer).ok();
-                scheme.bg_color = str_to_color(buffer.trim());
-            }
-            "2" => {
-                scheme.fancy_background = true;
-                buffer.clear();
-                println!("Choose your first color: red, green, blue: ");
-                io::stdin().read_line(&mut buffer).ok();
-                scheme.bg_color = str_to_color(buffer.trim());
-                buffer.clear();
-                println!("Choose one of the remaining two: red, green, blue: ");
-                io::stdin().read_line(&mut buffer).ok();
-                scheme.bg_color_2 = str_to_color(buffer.trim());
-                println!("Assummed good input...");
-            }
-            "quit" => finished = true,
-            _ => println!("Invalid input: {:?}. Enter a number (1, 2, ..)", buffer),
-        }
-        buffer.clear();
-    }
+    trimmed.to_string()
 }
 
 pub fn _randomize(_scheme: &mut Scheme) {}
