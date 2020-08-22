9cd7bd64a634471c3983233db172831fd3fe4d30
Simon Barton <Simon5@pdx.edu>
2020-08-22 11:33:46 -0700

    Working, images appear

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,43 @@ mod menu;
 mod util;
 
 use crate::auto_random::*;
+use rocket::response::NamedFile;
 use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+use std::path::Path;
 
 /// Start a web server and display a cool fractal on root page
 /// Allow user to send a GET to create a new fractal as much as they want
 #[get("/", format = "text/html")]
 fn index() -> Template {
-    let context = TemplateContext();
+    let mut context = HashMap::new();
+    context.insert("foo".to_string(), "bar".to_string());
     Template::render("index", &context)
 }
 
 #[get("/gen")]
 fn generate() -> Template {
     auto_random(1, "cool");
-    let context = TemplateContext();
+    let mut context = HashMap::new();
+    context.insert("foo".to_string(), "baz".to_string());
     Template::render("gen", &context)
 }
 
+#[get("/cool.png")]
+fn cool() -> Option<NamedFile> {
+    let path = Path::new("cool.png");
+    NamedFile::open(&path).ok()
+}
+
+#[get("/cool0.png")]
+fn cool0() -> Option<NamedFile> {
+    let path = Path::new("cool0.png");
+    NamedFile::open(&path).ok()
+}
+
 fn main() {
-    rocket::ignite().mount("/", rocket::routes![index, generate])
-    .attach(Template::fairing())
-    .launch();
+    rocket::ignite()
+        .mount("/", rocket::routes![index, generate, cool, cool0])
+        .attach(Template::fairing())
+        .launch();
 }
